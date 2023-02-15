22d45be0f9e0626caba5ac1e1f748505602987b7
felixbutzbach <felix@antena1.com.br>
2023-02-15 23:43:14 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,28 @@
+# Weekly Recap Helper -> WeReHe
+
+This project will is meant to practice prototyping and to play around with remix, typescript, tailwind, Prisma and maybe even testing and fly.io
+
+## Idea:
+1. Copy the content of the last Trello weekly recap card description (markup) and insert it into the werehe tool.
+2. It will parse the content and show it in an easy to navigate way where all of the following options can be done on every task:
+  - Add a new update
+  - Mark as Done
+3. It should also be possible to insert new tasks.
+4. Once all items have been added or edited, the tool can export the new list as markup which then can be inserted into the next weekly recap card.
+5. When exporting, the following steps should be taken
+  - Move the cards to the correct sections (edited cards will move to TAREFAS EM ANDAMENTO, New cards will be added to NOVAS TAREFAS)
+  - Add the current date to all cards that have been edited or added
+6. Export with markup styling
+
+
+## Log:
+- 06.02.23, 20:15: Started
+- 06.02.23, 22:30: Installed and followed the remix-indie-stack example project. Learned a bit about the basics of Remix, typescript and Prisma
+- 13.02.23, 21:05 - 22:45: Create a Vite app to get started quickly, created the first draft of the data input and main screens, lost a lot of time with form input value retrieval, first test with markdown parser
+- 14.02.23, 12:50 - 13:50: Implemented the basic parsing logic
+- 14.02.23, 22:15 - 23:15: Better rendering, including the children
+- 14.02.23, 23:30 - 00:15: Implemented the export function
+- 15.02.23, 12:15 - 13:00: Implemented the add new child function
+- 15.02.23, 21:00 - 23:15: Added removal of newly added child and enter listener, added addItem functionality, added removeItem functionality
+- Next: Deploy to stackblitz? Or try remix fly or however it is called
+
---
+++ src/App.tsx
@@ -0,0 +1,310 @@
+import React, { useState } from 'react'
+import MarkdownIt from 'markdown-it';
+const md = new MarkdownIt('commonmark');
+import './App.css'
+
+type TComment = {
+  content: string,
+  isNew: boolean
+}
+type TItem = {
+  id: string,
+  title: string,
+  comments: Array<TComment>
+  isNew: boolean,
+}
+
+const parseContent = (rawContent: string) => {
+
+  // Clean out some common whitespaces (found here: https://marked.js.org/)
+  const cleanedRawContent = rawContent.replace(/^[\u200B\u200C\u200D\u200E\u200F\uFEFF]/, "");
+
+  const markDownContent = md.parse(cleanedRawContent, {})
+
+  const newParsedContent: Array<TItem> = [];
+  let currentIndex = 0;
+
+  markDownContent.forEach((element, index) => {
+    // Extract all inline elements (type=inline) that actually have content (content.length > 1)
+    // Remove headers by checking previous element and see if it is type = heading_open
+
+    if (element.type === 'inline' && element.content && element.content.length > 1) {
+      // Found an inline element that is not empty
+      // Check if it is a header
+      if (markDownContent[index - 1] && markDownContent[index - 1].type === "heading_open") {
+        // Its a header
+        // Currently not used
+        // console.log('found a header:', markDownContent[index-1].tag, element.content);
+      } else {
+        // Its some (real) content
+        // console.log('found some content', element.content);
+        // console.log('comments', element.comments);
+        // Loop over the comments to get all content
+        // @TODO extract more information from the comments?
+        const newItem: TItem = {
+          id: String(currentIndex),
+          title: "",
+          comments: [],
+          isNew: false,
+        }
+        const newComments: Array<TComment> = [];
+        element.children?.forEach((child, index) => {
+          if (child.type === 'text') {
+            if (index === 0) {
+              // Thats the title
+              newItem.title = child.content.replace(/\[UPDATED\] ?/, '') // Remove the [UPDATED] tag, if any
+            } else {
+              newComments.push({ content: child.content, isNew: false })
+            }
+          }
+        });
+        newParsedContent.push({
+          ...newItem,
+          comments: newComments
+        })
+        currentIndex++;
+      }
+    }
+  })
+
+  console.log('newParsedContent', newParsedContent);
+
+  return newParsedContent;
+}
+
+const convertToMarkdown = (parsedContent: Array<TItem>) => {
+  // Create new headers 'doing' and 'new'
+  // add all itens as sub headers
+  // add all comments as list
+  let exportString = '# Tarefas em andamento:\n---\n';
+  let isNew = false;
+
+  parsedContent.forEach(item => {
+    if(item.isNew !== isNew){
+      // Now the new items
+      exportString += '# Tarefas novas:\n---\n';;
+      isNew = true;
+    }
+    exportString += `- ${item.title}\n`;
+    item.comments.forEach(comment => {
+      exportString += `  ${comment.content}\n`;
+    })
+    exportString += '---\n'
+  });
+
+  return exportString;
+}
+
+const createDateTag = () => {
+  const now = new Date();
+  const day = ('0' + now.getDate()).slice(-2);
+  const month = ('0' + now.getMonth()).slice(-2);
+  const year = now.getFullYear().toString().slice(-2);
+
+  return `[${day}.${month}.${year}]`
+}
+
+const Item = ({ item, updateItem, removeItem }: { item: TItem, updateItem: Function, removeItem: Function }) => {
+
+  const [newCommentContent, setNewCommentContent] = useState('')
+
+  const handleAddNewComment = () => {
+    if (newCommentContent.length === 0) {
+      return alert('New comment is empty');
+    }
+    const newComments = [...item.comments];
+    newComments.push({ content: createDateTag() + ' ' + newCommentContent, isNew: true })
+    setNewCommentContent('');
+    updateItem({ ...item, comments: newComments })
+  }
+
+  const handleRemoveComment = () => {
+    updateItem({ ...item, comments: item.comments.filter(comment => !comment.isNew) })
+  }
+
+  const handleRemoveItem = () => {
+    removeItem(item)
+  }
+
+  return (
+    <li key={item.id}>
+      <h2 className="item-title">{item.title}{item.isNew && <button type="button" className="inline-button delete" onClick={handleRemoveItem}>Delete</button>}</h2>
+      {
+        <ol className="item-container">
+          {item.comments.map((comment, index) => {
+            return <li className={`item-element ${index === 0 ? 'description' : ''}`} key={`${item.id}-${index}`}>
+              {
+                index === 0 ?
+                  <>{comment.content}</> :
+                  <>{`${index}: ${comment.content}`}{comment.isNew && <button type="button" className="inline-button delete" onClick={handleRemoveComment}>Delete</button>}</>
+              }
+            </li>
+          })}
+          {
+            !item.comments.find((comment => comment.isNew)) &&
+            <li key={`${item.id}-new`} className="comment-new">
+              <label htmlFor={`new-comment-${item.id}`}>New Comment</label><input id={`new-comment-${item.id}`} type="text" value={newCommentContent} onChange={(e) => setNewCommentContent(e.target.value)} onKeyDown={(e) => e.key === 'Enter' && handleAddNewComment()} />
+              {newCommentContent.length > 0 && <button type="button" className="inline-button add" onClick={handleAddNewComment}>Add</button>}
+            </li>
+          }
+        </ol>
+      }
+    </li>)
+}
+
+const NewItemForm = ({ addItem }: { addItem: Function }) => {
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+
+  const handleAddNewItem = () => {
+    if (title.length === 0) {
+      return alert('Title is empty');
+    }
+    if (description.length === 0) {
+      return alert('Description is empty');
+    }
+    // create a new Item by using the title and the description
+    const newItem: TItem = {
+      id: 'Will be set in addItem',
+      title: `${title} ${createDateTag()}`,
+      comments: [{ content: `-> ${description}`, isNew: true }],
+      isNew: true
+    }
+
+    setTitle('')
+    setDescription('')
+    addItem(newItem)
+  }
+
+  return (
+    <div className="item-new-form">
+      <p>Add new item</p>
+      <label htmlFor='title' className="item-new-title">Title<input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} /></label>
+      <label htmlFor='description' className="item-new-description">Description<input type="text" id="description" value={description} onChange={(e) => setDescription(e.target.value)} onKeyDown={(e) => e.key === 'Enter' && handleAddNewItem()} /></label>
+      {
+        (title.length > 0 && description.length > 0) && 
+        <button type="button" onClick={handleAddNewItem}>Add</button>
+      }
+    </div>
+  )
+}
+
+function App() {
+  const [rawContent, setRawContent] = useState('')
+  const [copyButtonText, setCopyButtonText] = useState('Copy')
+  const [parsedContent, setParsedContent] = useState<false | Array<TItem>>(false)
+  const [exportContent, setExportContent] = useState<false | string>(false)
+
+  // convert the raw content to structured data
+  const handelParseInput = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const newParsedContent = parseContent(rawContent);
+
+    setParsedContent(newParsedContent)
+  }
+
+  // convert the parsedContent into markdown
+  // and show it in the textarea
+  const handleExportButton = (e: React.MouseEvent) => {
+    if (parsedContent) {
+      const exportContent = convertToMarkdown(parsedContent);
+      setExportContent(exportContent)
+    }
+  }
+
+  // Go back to the input textarea
+  // Reset parsed and export content
+  const handleBackButton = (e: React.MouseEvent) => {
+    setParsedContent(false);
+    setExportContent(false);
+  }
+
+  // clear the input area
+  const handleResetButton = (e: React.MouseEvent) => {
+    setRawContent('')
+  }
+
+  // Copy export content to clipboard
+  const handleCopyButton = (e: React.MouseEvent) => {
+    if (exportContent) {
+      navigator.clipboard.writeText(exportContent)
+      setCopyButtonText('Copied!')
+      setTimeout(() => setCopyButtonText('Copy'), 1000)
+    }
+  }
+
+  const updateItem = (updatedItem: TItem) => {
+    if (parsedContent) {
+      const newParsedContent = parsedContent.map(item => {
+        if (item.id === updatedItem.id) {
+          return { ...updatedItem, title: `[UPDATED] ${updatedItem.title}` };
+        }
+        return { ...item }
+      })
+
+      setParsedContent(newParsedContent)
+    }
+  }
+
+  const addItem = (newItem: TItem) => {
+    if (parsedContent) {
+      const newParsedContent = [...parsedContent]
+      newParsedContent.push({ ...newItem, id: String(parsedContent.length) })
+      setParsedContent(newParsedContent)
+    }
+  }
+  
+  const removeItem = (itemToRemove: TItem) => {
+    if (parsedContent) {
+      const newParsedContent = [...parsedContent]
+      setParsedContent(newParsedContent.filter(item => item.id !== itemToRemove.id))
+    }
+  }
+
+
+  return (
+    <div className="App">
+      <h1>Weekly Recap Helper</h1>
+      {
+        !parsedContent ?
+          <>
+            <p>Insert Markup below and click import or hit enter</p>
+            <form className="form-input" onSubmit={handelParseInput}>
+              <label htmlFor="input-box"></label>
+              <textarea className="textarea-input" name="input-box" id="input-box" rows={20} cols={60} value={rawContent} onChange={(e) => setRawContent(e.target.value)} />
+              <div className="button-container">
+                {rawContent.length > 0 && <button type="button" onClick={handleResetButton}>Reset</button>}
+                <button disabled={rawContent.length === 0}>Import</button>
+              </div>
+            </form>
+          </>
+          :
+          <>
+            <p>Edit Elements below, when ready press Export to copy content to clipboard</p>
+            <p>{`${parsedContent.length} items have been found.`}</p>
+            <ul className="content-container">
+              {parsedContent.map(item => <Item key={item.id} item={item} updateItem={updateItem} removeItem={removeItem} />)}
+            </ul>
+            <NewItemForm addItem={addItem} />
+            <hr />
+            <div className="button-container">
+              <button onClick={handleBackButton}>Back</button>
+              <button onClick={handleExportButton}>Export</button>
+            </div>
+            {
+              exportContent &&
+              <>
+                <textarea className="textarea-export" name="export-box" id="export-box" rows={20} cols={60} value={exportContent} onChange={(e) => setExportContent(e.target.value)} />
+                <div className="button-container">
+                  <button onClick={handleCopyButton}>{copyButtonText}</button>
+                </div>
+              </>
+            }
+          </>
+      }
+    </div >
+  )
+}
+
+export default App
