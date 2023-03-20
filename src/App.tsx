09b3bc45092b2221e64d993dfa76e05f7302c84f
Felix <felix@antena1.com.br>
2023-03-20 16:01:29 +0100

    Fix month off by 1 error
    Not tested, but I am pretty sure it will work

--- src/App.tsx
+++ src/App.tsx
@@ -99,7 +99,7 @@ const convertToMarkdown = (parsedContent: Array<TItem>) => {
 const createDateTag = () => {
   const now = new Date();
   const day = ('0' + now.getDate()).slice(-2);
-  const month = ('0' + now.getMonth()).slice(-2);
+  const month = ('0' + now.getMonth() + 1).slice(-2);
   const year = now.getFullYear().toString().slice(-2);
 
   return `[${day}.${month}.${year}]`
